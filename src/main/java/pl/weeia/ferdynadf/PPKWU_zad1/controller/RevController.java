7b6ceac6519c8671e8b65aacf44f8b50931ae3ff
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-10-15 16:52:28 +0200

    delete stringApi method

--- src/main/java/pl/weeia/ferdynadf/PPKWU_zad1/controller/RevController.java
+++ src/main/java/pl/weeia/ferdynadf/PPKWU_zad1/controller/RevController.java
@@ -2,14 +2,20 @@ package pl.weeia.ferdynadf.PPKWU_zad1.controller;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ */
 @RestController
-public class zadanie1 {
+@RequestMapping("api")
+public class RevController {
 
     @GetMapping(value = "/rev")
-    public String reverseString(@RequestParam(name ="string") String givenString){
+    public String reverseString(@RequestParam(name ="string", defaultValue = "HelloWorld") String givenString){
         return StringUtils.reverse(givenString);
     }
+
 }
